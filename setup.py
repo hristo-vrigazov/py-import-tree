72ad7f9dffc9b5d33e0a38e1083556bc06cf65e1
Hristo Vrigazov <hvrigazov@gmail.com>
2021-12-11 12:19:56 +0200

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import pathlib
+import py_import_tree
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="py_import_tree",
+    version=py_import_tree.__version__,
+    description="DNN.Cool: Multi-task learning for Deep Neural Networks (DNN).",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hristo-vrigazov/py-import-tree",
+    author="Hristo Vrigazov",
+    author_email="hvrigazov@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent"
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["pandas"
+                      "numpy",
+                      "stdlib_list",
+                      "joblib"],
+    extras_require={}
+)
